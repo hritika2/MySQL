1209ea09b9055da3c1966f945bc4535e10ebf157
hritika2 <110270781+hritika2@users.noreply.github.com>
2023-03-02 23:31:10 +0530

    Add files via upload

---
+++ data_analysis_through_SQL.sql
@@ -0,0 +1,11 @@
+USE db1;
+
+SELECT branch, customer_type, payment, gross_income FROM sales_data ORDER BY gross_income DESC ;
+
+SELECT eeid, performance_review FROM employee_data WHERE performance_review >= 3 ORDER BY performance_review DESC;
+
+SELECT Unit_price, Quantity, Unit_price * Quantity AS Avg_Monthly_Sales FROM sales_data ORDER BY Avg_Monthly_Sales DESC;
+
+SELECT DISTINCT Branch, City FROM sales_data;
+
+
